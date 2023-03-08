3feea59ad3947873a5edb6931da896275a07f619
Vale Quiroga <90653297+ValeQRamos@users.noreply.github.com>
2023-03-08 14:16:26 -0600

    adding gasp and animation to cards

--- src/views/QuizesView.vue
+++ src/views/QuizesView.vue
@@ -2,15 +2,37 @@
 import q from "../data/data.json";
 import { ref, watch } from "vue";
 import Card from "../components/Card.vue";
+import gsap from "gsap";
 
 const quizzes = ref(q);
 const search = ref("");
+const showCards = ref(false);
 
 watch(search, () => {
   quizzes.value = q.filter((quiz) =>
     quiz.name.toLowerCase().includes(search.value.toLowerCase())
   );
 });
+
+const beforeEnter = (el) => {
+  // card-enter-from
+  el.style.opacity = 0;
+  el.style.transform = "translateY(-150px)";
+};
+
+const enter = (el) => {
+  // card-enter-to
+  gsap.to(el, {
+    y: 0,
+    opacity: 1,
+    duration: 0.3,
+    delay: el.dataset.index * 0.3,
+  });
+};
+
+const afterEnter = (el) => {
+  // card-enter-active
+};
 </script>
 
 <template>
@@ -20,7 +42,19 @@ watch(search, () => {
       <input type="text" placeholder="Search..." v-model.trim="search" />
     </header>
     <div class="options-container">
-      <Card v-for="quiz in quizzes" :key="quiz.id" :quiz="quiz" />
+      <TransitionGroup
+        appear
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @after-enter="afterEnter"
+      >
+        <Card
+          v-for="(quiz, index) in quizzes"
+          :key="quiz.id"
+          :quiz="quiz"
+          :data-index="index"
+        />
+      </TransitionGroup>
     </div>
   </div>
 </template>
@@ -50,6 +84,4 @@ header input {
   flex-wrap: wrap;
   margin-top: 40px;
 }
-
-/* CARD */
 </style>
